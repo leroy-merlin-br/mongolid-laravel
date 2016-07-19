129cbfa16ca58bd40eb4b94bc5c8e46467468550
Ravan Scafi <rscafi@gmail.com>
2016-07-19 00:26:13 -0300

    Adding tests to MongolidServiceProvider

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -12,25 +12,4 @@ class TestCase extends Orchestra\Testbench\TestCase
         parent::setUp();
         Ioc::setContainer($this->app);
     }
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function getPackageProviders($app)
-    {
-        return [
-            \MongolidLaravel\MongolidServiceProvider::class,
-        ];
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function getPackageAliases($app)
-    {
-        return [
-            'MongoLid'  => \MongolidLaravel\MongolidModel::class,
-            'Validator' => \Illuminate\Support\Facades\Validator::class,
-        ];
-    }
 }
