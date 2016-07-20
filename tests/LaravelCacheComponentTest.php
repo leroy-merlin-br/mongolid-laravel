01fc76fed1594227f513f77b857c9ec4463b43b7
Zizaco Zizuini <zizaco@gmail.com>
2016-07-20 11:52:21 -0300

    Added tests for the newly LaravelCacheComponent

---
+++ tests/LaravelCacheComponentTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace MongolidLaravel;
+
+use Mockery as m;
+use Illuminate\Cache\Repository;
+use Mongolid\Serializer\Serializer;
+use TestCase;
+
+class LaravelCacheComponentTest extends TestCase
+{
+    public function testShouldGet()
+    {
+        // Set
+        $cacheRepo  = m::mock(Repository::class);
+        $serializer = m::mock(Serializer::class);
+        $component  = new LaravelCacheComponent($cacheRepo, $serializer);
+        $key        = 'foo';
+        $value      = 'bar';
+
+        // Expectations
+        $cacheRepo->shouldReceive('get')
+            ->once()
+            ->with($key, null)
+            ->andReturn($value);
+
+        // Assertion
+        $this->assertEquals($value, $component->get($key));
+    }
+
+    public function testShouldPut()
+    {
+        // Set
+        $cacheRepo  = m::mock(Repository::class);
+        $serializer = m::mock(Serializer::class);
+        $component  = new LaravelCacheComponent($cacheRepo, $serializer);
+        $key        = 'foo';
+        $value      = [(object) ['name' => 'batata']];
+
+        // Expectations
+        $serializer->shouldReceive('convert')
+            ->once()
+            ->with([['name' => 'batata']])
+            ->andReturn([['name' => 'chips']]);
+
+        $cacheRepo->shouldReceive('put')
+            ->once()
+            ->with($key, [['name' => 'chips']], 3)
+            ->andReturn($value);
+
+        // Assertion
+        $component->put($key, $value, 3);
+    }
+
+    public function testShouldCheckIfHave()
+    {
+        // Set
+        $cacheRepo  = m::mock(Repository::class);
+        $serializer = m::mock(Serializer::class);
+        $component  = new LaravelCacheComponent($cacheRepo, $serializer);
+        $key        = 'foo';
+        $exists     = true;
+
+        // Expectations
+        $cacheRepo->shouldReceive('has')
+            ->once()
+            ->with($key)
+            ->andReturn($exists);
+
+        // Assertion
+        $this->assertEquals($exists, $component->has($key));
+    }
+}
