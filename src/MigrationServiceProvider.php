777627e9acc0f2786b263847d7b991bd02fd86d3
Ravan Scafi <rscafi@leroymerlin.com.br>
2018-09-28 14:33:53 -0300

    Copy MigrationServiceProvider

---
+++ src/MigrationServiceProvider.php
@@ -0,0 +1,86 @@
+<?php
+namespace MongolidLaravel;
+
+use Illuminate\Support\ServiceProvider;
+use MongolidLaravel\Migrations\Migrator;
+use MongolidLaravel\Migrations\MigrationCreator;
+use MongolidLaravel\Migrations\DatabaseMigrationRepository;
+
+class MigrationServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerRepository();
+
+        $this->registerMigrator();
+
+        $this->registerCreator();
+    }
+
+    /**
+     * Register the migration repository service.
+     *
+     * @return void
+     */
+    protected function registerRepository()
+    {
+        $this->app->singleton('migration.repository', function ($app) {
+            $collection = $app['config']['database.mongodb.default.migrations'];
+
+            return new DatabaseMigrationRepository($app['db'], $collection);
+        });
+    }
+
+    /**
+     * Register the migrator service.
+     *
+     * @return void
+     */
+    protected function registerMigrator()
+    {
+        // The migrator is responsible for actually running and rollback the migration
+        // files in the application. We'll pass in our database connection resolver
+        // so the migrator can resolve any of these connections when it needs to.
+        $this->app->singleton('migrator', function ($app) {
+            $repository = $app['migration.repository'];
+
+            return new Migrator($repository, $app['db'], $app['files']);
+        });
+    }
+
+    /**
+     * Register the migration creator.
+     *
+     * @return void
+     */
+    protected function registerCreator()
+    {
+        $this->app->singleton('migration.creator', function ($app) {
+            return new MigrationCreator($app['files']);
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [
+            'migrator', 'migration.repository', 'migration.creator',
+        ];
+    }
+}
