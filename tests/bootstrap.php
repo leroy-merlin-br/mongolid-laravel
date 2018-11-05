6025c4151907e5f3cedcfd4d3775be5fffea996c
Ravan Scafi <rscafi@leroymerlin.com.br>
2018-11-05 11:37:49 -0200

    Drop tests bootstrap file

--- tests/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace MongolidLaravel;
-
-require __DIR__.'/../vendor/autoload.php';
-
-use SebastianBergmann\Comparator\Factory;
-
-Factory::getInstance()->register(new UTCDateTimeComparator());
